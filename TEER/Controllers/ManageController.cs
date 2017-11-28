d02d11947b4f5fa4fd89249ab552bc1a38f12741
Pranay - MTA <pranayritvik@gmail.com>
2017-11-28 12:39:58 -0500

    First MVC5 Project

--- TEER/Controllers/ManageController.cs
+++ TEER/Controllers/ManageController.cs
@@ -18,6 +18,7 @@ namespace TEER.Controllers
 
         public ManageController()
         {
+
         }
 
         public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
