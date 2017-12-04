2a8c82b8a30dbe1293604f14b4087d3ddc96f453
Pranay <pranayritvik@gmail.com>
2017-12-04 11:13:16 -0500

    view changes

---
+++ TEER/Controllers/ComputerInformationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEER.Controllers
+{
+    public class ComputerInformationController : Controller
+    {
+        // GET: ComputerInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewComputerInformation()
+        {
+            ViewBag.Message = "Your View Computer Information page.";
+
+            return View();
+        }
+
+        public ActionResult ViewLocationDetails()
+        {
+            ViewBag.Message = " Your View Location Details Page.";
+            return View();
+        }
+    }
+}
