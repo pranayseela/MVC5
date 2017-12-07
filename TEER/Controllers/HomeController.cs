f8b37c9c4817caaa0c9c928fd4a9d6a1b7b10ef2
Pranay <pranayritvik@gmail.com>
2017-12-07 14:22:49 -0500

    Added Disclaimer page

--- TEER/Controllers/HomeController.cs
+++ TEER/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace TEER.Controllers
 {
@@ -12,20 +15,15 @@ namespace TEER.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
-
     }
 }
