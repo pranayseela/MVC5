ef94ea668c3d772bbb98dbe8ace88123b132bd40
Pranay <pranayritvik@gmail.com>
2017-12-11 11:48:16 -0500

    Added Computer information class files

--- TEER/Controllers/RegisterController.cs
+++ TEER/Controllers/RegisterController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TEER.Controllers
 {
@@ -16,7 +18,22 @@ namespace TEER.Controllers
 
         public ActionResult RegisterSheet()
         {
+            string ipAddress = GetLocalIPAddress();
+            ViewBag.ipAddress = ipAddress;
             return View();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
